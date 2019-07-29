ce7c91023bdec13095aefc3db18130fcfdfd8f56
unknown <1322122798@qq.com>
2019-07-29 18:38:20 +0800

    s石起民第一次提交交

--- src/Router/index.js
+++ src/Router/index.js
@@ -5,6 +5,7 @@ import Home from '../Views/Home'
 import Cart from '../Views/Cart'
 import Category from '../Views/Category'
 import User from '../Views/User'
+import Detail from '../Views/Detail'
 const router = <Router>
     <App>
         <Switch>
@@ -12,6 +13,7 @@ const router = <Router>
             <Route path='/category' component={Category} exact></Route>
             <Route path='/cart' component={Cart} exact></Route>
             <Route path='/user' component={User} exact></Route>
+            <Route path='/detail:id' component={Detail} exact></Route>
         </Switch>
     </App>
 </Router>
